7a36c8a770ad252f47e424bb2d7a239edfa9b22f
ANIKMONDALB <158838149+ANIKMONDALB@users.noreply.github.com>
2024-07-11 11:41:31 +0530

    Update config-env.js

--- config-env.js
+++ config-env.js
@@ -9,8 +9,8 @@ function convertToBool(text, fault = 'true') {
 const username = fs.readFileSync('github_username.txt', 'utf8').trim()
 
 module.exports = {
-    SESSION_ID:  process.env.SESSION_ID,    
-    BOT_NUMBER:  process.env.BOT_NUMBER,
-    GITHUB_USERNAME: username,
-    GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN,
+    SESSION_ID:  process.env.SESSION_ID, PRABATH-MD~fFUyjTpJ#Cx8gmkE1Rwhl-KaGLVj0ez0A6G7Onwcjlnjj05UisbE   
+    BOT_NUMBER:  process.env.BOT_NUMBER,918617340627
+    GITHUB_USERNAME: username,ANIKMONDALB
+    GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN,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
 };
